aae1902ae71699040c855c6df28195cbc2250b01
Stykolar <43656005+Stykolar@users.noreply.github.com>
2018-10-30 22:46:30 -0400

    Started work on weapon swapping
    Abstract class called "Weapon" is used in conjunction with an array. In the sample scene for weapon swapping the key "1" uses the firing of a quick gatling gun whilst "2" changes to a slower version.

---
+++ Assets/Scripts/WeaponStuff/GatlingGun.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GatlingGun : Weapon
+{
+	public GameObject bullet, barrelL, barrelR;
+	private bool rightArm = true;
+
+	public override void fire()
+	{
+		if (!canFire)
+			return;
+
+		GameObject clone;
+
+		if (rightArm)
+		{
+			clone = Instantiate(bullet, barrelR.transform.position, transform.rotation) as GameObject;
+			rightArm = false;
+		}
+		else
+		{
+			clone = Instantiate(bullet, barrelL.transform.position, transform.rotation) as GameObject;
+			rightArm = true;
+		}
+
+		counter = fireRate;
+	}
+}
---
+++ Assets/Scripts/WeaponStuff/WeaponManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+ 
+
+public class WeaponManager : MonoBehaviour
+{
+	public Weapon[] weapons;
+	public int activeWeapon;
+
+	// makes it easier to look through inputs
+	private KeyCode[] keyCodes =
+	{
+		KeyCode.Alpha1,
+		KeyCode.Alpha2,
+		KeyCode.Alpha3,
+		KeyCode.Alpha4,
+		KeyCode.Alpha5,
+		KeyCode.Alpha6,
+		KeyCode.Alpha7,
+		KeyCode.Alpha8,
+		KeyCode.Alpha9
+	};
+
+	// Use this for initialization
+	void Start ()
+	{
+		activeWeapon = 0;
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (Input.GetButton("Fire1"))
+		{
+			weapons[activeWeapon].fire();
+		}
+
+		for (int i = 0; i < weapons.Length; i++)
+		{
+			if (Input.GetKeyDown(keyCodes[i]))
+			{
+				activeWeapon = i;
+				break;
+				// does all the stuff with hiding the old weapon and showing the new one
+			}
+		}
+	}
+}
